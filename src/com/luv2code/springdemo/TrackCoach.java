e6dd2c36543464a0754b47d4466ecb1eabf2cba0
JustTryIt <justtryit7.2021@gmail.com>
2022-05-31 15:01:32 +0900

    48강 실습

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -20,6 +20,21 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
+	
+	//실행할 사용자 정의 코드
+	//bean이 만들어지거나 파괴되는 동안에 우리가 사용할 사용자 정의 코드
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+		
+	}
+	
 
 }
 
