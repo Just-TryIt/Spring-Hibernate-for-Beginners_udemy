20f00ac8d88068ec77de8a7fb2880762fe7fe539
JustTryIt <justtryit7.2021@gmail.com>
2022-06-06 17:39:39 +0900

    *56강 :  explicit bean id -> defalut bean id 실습

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,10 +2,10 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-//bean id : thatSillyCoach
-@Component("thatSillyCoach") 
+@Component
 public class TennisCoach implements Coach {
 
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
