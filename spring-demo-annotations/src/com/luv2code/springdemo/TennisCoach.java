803491600dd5e72aee52f7d6c03879755c8082be
JustTryIt <justtryit7.2021@gmail.com>
2022-06-09 15:36:36 +0900

    * 81강 : @PostConstruct, @PreDestroy 실습

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -17,6 +19,21 @@ public class TennisCoach implements Coach {
 	public TennisCoach() {
 		System.out.println(" >> TennisCoach :  inside default constructor ");
 	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach : inside of doMyStartupStuff() "
+				+ "-> @PostConstruct used");
+	}
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach : inside of doMyCleanupStuff()"
+				+ "-> @PreDestroy used");
+	}
+	
 	
 	
 	/*
