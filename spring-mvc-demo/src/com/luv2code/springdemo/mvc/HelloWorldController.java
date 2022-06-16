b1a2853a6a06a41c5b867e68a376cf477256d99d
JustTryIt <justtryit7.2021@gmail.com>
2022-06-16 16:26:52 +0900

    * 113강 : Reading HTML Form Data 실습2

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -10,11 +10,17 @@ public class HelloWorldController {
 	
 	@RequestMapping("/showForm")
 	public String showForm() {
-		return "helloworld-form";
+		return "helloworld-form"; // helloworld-form.jsp 페이지로 이동
 	}
 	
 	
 	
 	// need a controller method to process the HTML form
 
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld"; // helloworld.jsp 페이지로 이동
+	}
+	
+	
 }
